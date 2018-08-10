14ead3797bb0bbe7a2d478b3011af3f1cf3f6073
John Tillman <john.w.tillman@gmail.com>
2018-08-10 14:52:23 -0400

    final edits

--- liri.js
+++ liri.js
@@ -1,14 +1,25 @@
 // import packages
 require('dotenv').config();
-require('colors');
+const fs = require('fs');
 const keys = require('./keys');
 const request = require('request');
 const Twitter = require('twitter');
 const Spotify = require('node-spotify-api');
+const { Console } = require('console');
 
 //set keys
 const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
+ 
+//customer logger
+const output = fs.createWriteStream('./log.txt', {flags: 'a'});
+const errorOutput = fs.createWriteStream('./errorLog.txt', {flags: 'a'});
+const logger = new Console({ stdout: output, stderr: errorOutput });
+const customLog = (d) => {
+  logger.log(d);
+  console.log(d);
+}
+
 
 //command line arguments
 const command = process.argv[2];
@@ -17,63 +28,90 @@ const arg1 = process.argv[3];
 //command logic
 switch (command) {
   case 'my-tweets':
-    client.get('statuses/home_timeline', function(error, tweets, response) {
-      if(!error) {
-        for(let tweet of tweets) {
-          console.log(tweet.text);
-          console.log(tweet.created_at);
-          console.log(tweet.user.screen_name);
-        }
-      } else {
-        console.log(error);
-      }
-    });
+    getTweets();
     break;
     
   case 'spotify-this-song':
-    const query = arg1 ? arg1 : "All the Small Things"
-    spotify.search({ type: 'track', query }, function(error, data) {
-      if(!error) {
-        const firstTrack = data.tracks.items[0];
-        const track = firstTrack.name;
-        const artist = firstTrack.artists[0].name;
-        console.log(track);
-        console.log(artist);
-      } else {
-        console.log(error);
+    let query = '';
+    if(arg1) {
+      for(let i = 3; i < process.argv.length; i++) {
+        i === 3 ? query += process.argv[i] : query += " " + process.argv[i];
       }
-    });
+    } else {
+      query = "The Sign";
+    }
+    spotifySearch(query);
     break;
     
   case 'movie-this':
-    const apikey = 'trilogy';
-    let movie = '';
-    for(let i = 3; i < process.argv.length; i++) {
-      i === 3 ? movie = movie + process.argv[i] : movie = movie + "+" + process.argv[i];
-    }
-    const url = 'http://www.omdbapi.com/?apikey=' + apikey + '&t=' + movie;
-    console.log(url)
-    request(url, function (error, response, body) {
-      if(!error && response.statusCode === 200) {
-        const movieData = JSON.parse(body);
-        console.log("Title: ".green + movieData.Title);
-        console.log("Released: ".green + movieData.Released);
-        console.log(movieData.Ratings[0].Source.green + " score: ".yellow + movieData.Ratings[0].Value);
-        console.log(movieData.Ratings[1].Source.green + " score: ".yellow + movieData.Ratings[0].Value);
-        console.log("Country: ".green + movieData.Country);
-        console.log("Language: ".green + movieData.Language);
-        console.log("Plot: ".green + movieData.Plot);
-        console.log("Actors: ".green + movieData.Actors);
-      } else {
-        console.log(error);
-      }  
-    });
+    findMovie();
     break;
     
   case 'do-what-it-says':
-    
+    fs.readFile('random.txt', 'utf8', function(error, data) {
+      if(!error) {
+        spotifySearch(data);
+      } else {
+        console.log(error)
+      }
+    });
     break;  
 
   default:
     break;
+}
+
+function spotifySearch(query) {
+  spotify.search({ type: 'track', query }, function(error, data) {
+    if(!error) {
+      const track = query === 'The Sign' ? data.tracks.items[5] : data.tracks.items[0];
+      customLog('');
+      customLog(track.name);
+      customLog(track.artists[0].name);
+      customLog(track.album.name);
+      customLog(track.external_urls.spotify);
+    } else {
+      console.log(error);
+    }
+  });
+}
+
+function findMovie() {
+  const apikey = 'trilogy';
+    let movie = '';
+    for(let i = 3; i < process.argv.length; i++) {
+      i === 3 ? movie += process.argv[i] : movie += "+" + process.argv[i];
+    }
+    const url = 'http://www.omdbapi.com/?apikey=' + apikey + '&t=' + movie;
+    request(url, function (error, response, body) {
+      if(!error && response.statusCode === 200) {
+        const movieData = JSON.parse(body);
+        customLog('');
+        customLog("Title: " + movieData.Title);
+        customLog("Released: " + movieData.Released);
+        customLog(movieData.Ratings[0].Source + " score: " + movieData.Ratings[0].Value);
+        customLog(movieData.Ratings[1].Source + " score: " + movieData.Ratings[0].Value);
+        customLog("Country: " + movieData.Country);
+        customLog("Language: " + movieData.Language);
+        customLog("Plot: " + movieData.Plot);
+        customLog("Actors: " + movieData.Actors);
+      } else {
+        console.log(error);
+      }  
+    });
+}
+
+function getTweets() {
+  client.get('statuses/user_timeline', function(error, tweets, response) {
+    if(!error) {
+      for(let tweet of tweets) {
+        customLog('');
+        customLog(tweet.text);
+        customLog(tweet.created_at);
+        customLog(tweet.user.screen_name);
+      }
+    } else {
+      console.log(error);
+    }
+  });
 }
